0b31c6ca96511594677665670f3c8dbdafaa24be
root <root@datenlager.local>
2023-11-11 12:16:18 +0000

    MDL3 YunoHost

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Moodle 3.11 for YunoHost
+
+> *This package allows for a fast installation of Moodle version 3 on a YunoHost server with all the necessary plugins for deployment at HSD.*
+
+## Overview
+
+A Moodle package version 3.11 for setting up a small Moodle archive at Düsseldorf University of Applied Sciences.
+
+**Shipped version:** 3.11.17~ynh1
+
+## Documentation and resources
+
+* Official app website: <https://moodle.org/>
+* Official admin documentation: <https://docs.moodle.org/400/en/Main_page>
+* Upstream app code repository: <https://github.com/moodle/moodle>
---
+++ scripts/_common.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# Function to clone and install a Moodle plugin from a Git repository
+install_plugin() {
+    local DIR=$1
+    local GIT_URL=$2
+    local VERSION=$VERSION
+    local GIT_BRANCHES=("MOODLE_${VERSION}_STABLE" "MOODLE_$((${VERSION} - 1))_STABLE" "MOODLE_$((${VERSION} - 2))_STABLE")
+
+    echo "Attempting to install plugin in ${DIR} from ${GIT_URL}"
+
+    # Check if the module is already installed
+    if [ ! -d "${install_dir}/${DIR}" ]; then
+        # Clone the module from the first available branch
+        for BRANCH in "${GIT_BRANCHES[@]}"; do
+            if git ls-remote "$GIT_URL" | grep -sw "$BRANCH" > /dev/null 2>&1; then
+                echo "Cloning branch '$BRANCH'"
+                git clone -b "$BRANCH" "$GIT_URL" "${install_dir}/${DIR}"
+                break
+            fi
+        done
+
+        # If no specific branch was cloned, clone the default branch
+        if [ ! -d "${install_dir}/${DIR}" ]; then
+            echo "No stable branch found. Cloning default branch."
+            git clone "$GIT_URL" "${install_dir}/${DIR}"
+        fi
+
+        # Remove .git directory
+        rm -rf "${install_dir}/${DIR}/.git"
+    else
+        echo "Plugin ${DIR} is already installed."
+    fi
+}
+
+# Function to clone a Moodle theme from a Git repository
+install_theme() {
+    local DIR=$1
+    local GIT_URL=$2
+    local BRANCH=$3
+
+    echo "Attempting to install theme in ${install_dir}/${DIR} from ${GIT_URL}"
+
+    # Remove the existing theme directory if it exists
+    if [ -d "${install_dir}/${DIR}" ]; then
+        echo "Removing existing theme directory: ${install_dir}/${DIR}"
+        rm -rf "${install_dir}/${DIR}"
+    fi
+
+    # Clone the theme if the specified branch exists in the repository
+    if git ls-remote --heads "$GIT_URL" | grep -sw "refs/heads/$BRANCH" > /dev/null 2>&1; then
+        echo "Cloning branch '$BRANCH' from $GIT_URL into ${install_dir}/${DIR}"
+        git clone -b "$BRANCH" "$GIT_URL" "${install_dir}/${DIR}"
+    else
+        echo "Error: Branch '$BRANCH' does not exist in the repository."
+        return 1
+    fi
+}
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+install_moodle_plugins() {
+
+    # Extract branch from Moodle's version.php
+    VERSION=$(grep 'branch' "${install_dir}/version.php" | tail -n 1 | cut -d"'" -f2)
+    echo "Moodle branch extracted: $VERSION"
+
+    # Display progress message
+    ynh_script_progression --message="Installing plugins from git..." --weight=6
+
+    # Admin tools
+    install_plugin "admin/tool/inactive_user_cleanup" "https://github.com/dualcube/moodle-tool_inactive_user_cleanup.git"
+
+    # Local plugins
+    install_plugin "local/downloadcenter" "https://github.com/academic-moodle-cooperation/moodle-local_downloadcenter"
+    install_plugin "local/navbarplus" "https://github.com/moodle-an-hochschulen/moodle-local_navbarplus"
+    install_plugin "local/cohortrole" "https://github.com/paulholden/moodle-local_cohortrole"
+
+    # Mod plugins
+    install_plugin "mod/dialogue" "https://github.com/danmarsden/moodle-mod_dialogue"
+    install_plugin "mod/poster" "https://github.com/mudrd8mz/moodle-mod_poster"
+    install_plugin "mod/moodleoverflow" "https://github.com/learnweb/moodle-mod_moodleoverflow"
+    install_plugin "mod/offlinequiz" "https://github.com/academic-moodle-cooperation/moodle-mod_offlinequiz"
+    install_plugin "report/offlinequizcron" "https://github.com/academic-moodle-cooperation/moodle-report_offlinequizcron"
+    install_plugin "mod/margic" "https://github.com/coactum/moodle-mod_margic"
+    install_plugin "mod/publication" "https://github.com/academic-moodle-cooperation/moodle-mod_publication"
+    install_plugin "mod/etherpadlite" "https://github.com/moodlehu/moodle-mod_etherpadlite"
+    install_plugin "mod/lightboxgallery" "https://github.com/netspotau/moodle-mod_lightboxgallery"
+    install_plugin "mod/pcast" "https://github.com/sbourget/moodle-mod_pcast"
+    install_plugin "mod/questionnaire" "https://github.com/remotelearner/moodle-mod_questionnaire"
+    install_plugin "mod/subpage" "https://github.com/moodleou/moodle-mod_subpage"
+
+    # Microsoft plugins
+    install_plugin "local/o365" "https://github.com/Microsoft/moodle-local_o365"
+    install_plugin "auth/oidc" "https://github.com/Microsoft/moodle-auth_oidc"
+    install_plugin "lib/editor/atto/plugins/teamsmeeting" "https://github.com/enovation/moodle-atto_teamsmeeting"
+    install_plugin "repository/office365" "https://github.com/Microsoft/moodle-repository_office365"
+    install_plugin "theme/boost_o365teams" "https://github.com/Microsoft/moodle-theme_boost_o365teams"
+    install_plugin "blocks/microsoft" "https://github.com/Microsoft/moodle-block_microsoft"
+    install_plugin "filter/oembed" "https://github.com/PoetOS/moodle-filter_oembed"
+    install_plugin "local/office365" "https://github.com/Microsoft/moodle-local_office365"
+
+    # Question types
+    install_plugin "question/type/stack" "https://github.com/maths/moodle-qtype_stack"
+    install_plugin "question/behaviour/adaptivemultipart" "https://github.com/maths/moodle-qbehaviour_adaptivemultipart"
+    install_plugin "question/behaviour/dfcbmexplicitvaildate" "https://github.com/maths/moodle-qbehaviour_dfcbmexplicitvaildate"
+    install_plugin "question/behaviour/dfexplicitvaildate" "https://github.com/maths/moodle-qbehaviour_dfexplicitvaildate"
+
+    # Analytics
+    install_plugin "admin/tool/log/store/lanalytics" "https://github.com/rwthanalytics/moodle-logstore_lanalytics.git"
+    install_plugin "local/learning_analytics" "https://github.com/rwthanalytics/moodle-local_learning_analytics.git"
+
+    # HSD Themes for Moodle 3
+    install_theme "theme/hsd_blau" "git@gitlab.ruhr-uni-bochum.de:moodle-hsd/locally-hosted-submodules/hsd_themes.git" "hsd_blau"
+    install_theme "theme/hsd_blau2" "git@gitlab.ruhr-uni-bochum.de:moodle-hsd/locally-hosted-submodules/hsd_themes.git" "hsd_blau2"
+    install_theme "theme/hsd_gelb" "git@gitlab.ruhr-uni-bochum.de:moodle-hsd/locally-hosted-submodules/hsd_themes.git" "hsd_gelb"
+    install_theme "theme/hsd_gruen" "git@gitlab.ruhr-uni-bochum.de:moodle-hsd/locally-hosted-submodules/hsd_themes.git" "hsd_gruen"
+    install_theme "theme/hsd_rot" "git@gitlab.ruhr-uni-bochum.de:moodle-hsd/locally-hosted-submodules/hsd_themes.git" "hsd_rot"
+}
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
